5cf891394a0d806938d1b5dbddc546fa43c79e6b
Krizarmax <krizarmax@gmail.com>
2022-01-26 19:59:10 -0600

    Add files via upload

---
+++ correo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Funcion de validacion de comandos
+validar(){
+if [ $? -eq 0 ]; then
+	echo $1 OK 
+else
+	echo $1 Fail
+	exit 
+fi
+}
+telnet mail.kauil.com.mx 26 -l pruebas@kauil.com.mx <<EOF
+MAIL FROM: prueba@kauil.com.mx
+RCPT TO: $1
+DATA
+SUBJET: $2
+$3
+.
+quit
+EOF
+validar
